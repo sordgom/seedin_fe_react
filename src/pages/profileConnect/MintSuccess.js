5bae1b62e8133fa54d8294aa12e93fcd30f64d62
afizaaahhh <afizahazizaf@gmail.com>
2022-10-26 08:19:36 +0800

    updated bg connect

--- src/pages/profileConnect/MintSuccess.js
+++ src/pages/profileConnect/MintSuccess.js
@@ -1,20 +1,30 @@
 import React from "react";
+import bg from "../../assets/img/globe2.png";
 
 function MintSuccess() {
   return (
-    <div className="relative min-h-screen overflow-hidden bg-[#E5E5E5]">
-      <div className="flex flex-col justify-center items-center w-full mt-20">
-        <div className="w-[50%] px-8 py-[6rem] m-auto bg-[#DAFF3E] rounded-3xl font-robotoMono">
-          <div className="font-bold mt-[10rem] text-[5vh]">
-            Minting is Successful!
-          </div>
-          <div className="mt-[10rem]">
-            <button
-              type="button"
-              className="bg-black px-4 py-2 rounded-full font-bold text-[#DAFF3E]"
-            >
-              Back To Your Profile
-            </button>
+    <div className="relative h-screen overflow-hidden bg-black">
+      <div
+        className="w-full h-full fixed"
+        style={{
+          background: `url(${bg}) no-repeat bottom`,
+          backgroundSize: "cover",
+          backgroundOrigin: "content-box",
+        }}
+      >
+        <div className="flex flex-col justify-center items-center w-full mt-20">
+          <div className="w-[50%] px-8 py-[6rem] m-auto bg-[#DAFF3E] rounded-3xl font-robotoMono">
+            <div className="font-bold mt-[10rem] text-[5vh]">
+              Minting is Successful!
+            </div>
+            <div className="mt-[10rem]">
+              <button
+                type="button"
+                className="bg-black px-4 py-2 rounded-full font-bold text-[#DAFF3E]"
+              >
+                Back To Your Profile
+              </button>
+            </div>
           </div>
         </div>
       </div>
