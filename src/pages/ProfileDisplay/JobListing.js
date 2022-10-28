9cebd1a5bb20604bbfaa7265bbccbdc511fa4092
afifahathirah <afifahnajibah99@gmail.com>
2022-10-28 14:45:21 +0800

    joblisting done

--- src/pages/ProfileDisplay/JobListing.js
+++ src/pages/ProfileDisplay/JobListing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import imagesquare from "../../assets/img/imagesquare.png";
+import lightning from "../../assets/img/lightning.png";
+import dollarbag from "../../assets/img/dollarbag.png";
 
 function JobListing() {
   return (
@@ -8,19 +10,22 @@ function JobListing() {
         <form>
           <div>
             <div className="mb-2 text-left px-[6rem]">
-              <div className="flex flex-row">
-                <div className="text-2xl">
-                  <b>JOB LISTING</b>
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-col">
+                  <div className="text-2xl">
+                    <b>JOB LISTING</b>
+                  </div>
                 </div>
-                <div className="px-3">
-                  <button
-                    type="button"
-                    className="bg-white border-solid border-2 border-black text-[#000000] px-5 py-1 rounded-full font-bold"
-                  >
-                    List a Job
-                  </button>
-                </div>
-                <div>
+
+                <div className="flex flex-col">
+                  <div className="px-3">
+                    <button
+                      type="button"
+                      className="bg-white border-solid border-2 border-black text-[#000000] px-5 py-1 rounded-full font-bold"
+                    >
+                      List a Job
+                    </button>
+                  </div>
                   <button
                     type="button"
                     className="bg-black border-2 border-black text-[#DAFF3E] px-5 py-1 rounded-full"
@@ -33,89 +38,130 @@ function JobListing() {
             {/* --------------------------------------------------------------------------------------------------------------------------------------- */}
             <div className="flex flex-row mx-[5rem]">
               {/* First Box have CONTENT 1 */}
+
               <div className="flex flex-col px-4">
                 <div className="text-left text-base block px-7 py-6 mt-4 text-gray-700 bg-white rounded-[20px] focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
-                  <div className="">Maximoz Team</div>
-
-                  <div className="py-3 font-bold text-xl">Programmer</div>
-                  <div className="flex flex-col">
-                    <div>$14,000 - $25,000</div>
+                  <div className="flex flex-row justify justify-between">
+                    <div className="flex flex-col">
+                      <div>Maximoz Team</div>
+                      <div className="font-bold text-xl pt-2">
+                        Database Programmer
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                      <div>
+                        <img
+                          src={imagesquare}
+                          alt=""
+                          className="w-[60px] h-[60px]"
+                        />
+                      </div>
+                    </div>
                   </div>
 
-                  <div className="flex flex-col justify-end">
-                    <div>
-                      <img
-                        src={imagesquare}
-                        alt=""
-                        className="w-[60px] h-[60px]"
-                      />
-                    </div>
+                  <div className="flex flex-row justify-between py-3">
+                    <div>$14,000 - $25,000</div>
                     <div>London, England</div>
                   </div>
 
-                  <div className="pb-8">
+                  <div className="flex flex-row py-5 text-sm">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam...
                   </div>
-                  <div className="flex flex-row">
-                    <div className="pr-4">
+
+                  <div className="flex flex-row pt-4">
+                    <div className="pr-4 ">
                       <button
                         type="button"
-                        className="bg-[#DAFF3E] text-black px-5 py-1 rounded-full font-bold"
+                        className="bg-[#DAFF3E] text-black px-5 py-2 rounded-full font-bold flex"
                       >
                         FAST APPLY
+                        <img
+                          src={lightning}
+                          alt=""
+                          className="w-[20px] h-[20px]"
+                        />
                       </button>
                     </div>
                     <div>
                       <button
                         type="button"
-                        className="bg-black text-[#DAFF3E] px-5 py-1 rounded-full"
+                        className="bg-black text-[#DAFF3E] px-5 py-2 rounded-full font-bold flex"
                       >
+                        <img
+                          src={dollarbag}
+                          alt=""
+                          className="w-[20px] h-[20px]"
+                        />
                         REFER & GET N500
                       </button>
                     </div>
                   </div>
                 </div>
               </div>
-              {/* First Box have CONTENT 2 *
-              <div className="flex flex-col">
-                <div className="text-left block px-7 py-6 mt-4 text-gray-700 bg-white rounded-[20px] focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
-                  <div className="">Maximoz Team</div>
-                  <div className="py-3 font-bold text-xl">Programmer</div>
-                  <div className="flex flex-row pb-7">
+
+              {/* CONTENT 2 */}
+              <div className="flex flex-col px-4">
+                <div className="text-left text-base block px-7 py-6 mt-4 text-gray-700 bg-white rounded-[20px] focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                  <div className="flex flex-row justify justify-between">
                     <div className="flex flex-col">
-                      <div>$14,000 - $25,000</div>
+                      <div>Maximoz Team</div>
+                      <div className="font-bold text-xl pt-2">
+                        Database Programmer
+                      </div>
                     </div>
                     <div className="flex flex-col">
-                      <div className="text-right">London, England</div>
+                      <div>
+                        <img
+                          src={imagesquare}
+                          alt=""
+                          className="w-[60px] h-[60px]"
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div className="pb-8">
+
+                  <div className="flex flex-row justify-between py-3">
+                    <div>$14,000 - $25,000</div>
+                    <div>London, England</div>
+                  </div>
+
+                  <div className="flex flex-row py-5 text-sm">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam...
                   </div>
-                  <div className="flex flex-row">
+                  <div className="flex flex-row pt-4">
                     <div className="pr-4">
                       <button
                         type="button"
-                        className="bg-[#DAFF3E] text-black px-5 py-1 rounded-full font-bold"
+                        className="bg-[#DAFF3E] text-black px-5 py-2 rounded-full font-bold flex"
                       >
                         FAST APPLY
+                        <img
+                          src={lightning}
+                          alt=""
+                          className="w-[20px] h-[20px]"
+                        />
                       </button>
                     </div>
                     <div>
                       <button
                         type="button"
-                        className="bg-black text-[#DAFF3E] px-5 py-1 rounded-full"
+                        className="bg-black text-[#DAFF3E] px-7 py-2 rounded-full font-bold flex"
                       >
+                        <img
+                          src={dollarbag}
+                          alt=""
+                          className="w-[20px] h-[20px]"
+                        />
                         REFER & GET N500
                       </button>
                     </div>
                   </div>
                 </div>
-              </div> */}
+              </div>
             </div>
           </div>
         </form>
--- src/pages/ProfileDisplay/skills.js
+++ src/pages/ProfileDisplay/skills.js
@@ -6,20 +6,11 @@ function Skills() {
     <div className="px-[10rem] mt-8 font-robotoMono mx-auto">
       <div className="mb-2 text-left px-[6rem]">
         <form>
-          <div className="flex flex-row">
-            <div className="flex flex-col pr-[2rem]">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col ">
               <div className="pb-[1rem]">Main Skills</div>
               <div className="grid grid-cols-3 grid-flow-row gap-1">
-                <div className="bg-black border-2 border-black text-[#DAFF3E] px-5 py-1 rounded-full">
-                  Database Design
-                </div>
-                <div className="bg-black border-2 border-black text-[#DAFF3E] px-5 py-1 rounded-full">
-                  Database Design
-                </div>
-                <div className="bg-black border-2 border-black text-[#DAFF3E] px-5 py-1 rounded-full">
-                  Database Design
-                </div>
-                <div className="bg-black border-2 border-black text-[#DAFF3E] px-5 py-1 rounded-full">
+                <div className="bg-black border-2 border-black text-[#DAFF3E] px-2 rounded-full">
                   Database Design
                 </div>
               </div>
@@ -27,17 +18,17 @@ function Skills() {
             <div className="flex flex-col">
               <div className="pb-[1rem]">Other Skills</div>
               <div className="grid grid-cols-3 grid-flow-row gap-1">
-                <div className="bg-white border-solid border-2 border-black text-[#000000] p-auto rounded-full font-bold">
+                <div className=" flex justify-center items-center bg-white border-solid border-2 border-black text-[#000000] rounded-full font-bold">
+                  Smart Contract
+                </div>
+                <div className="flex justify-center items-center bg-white border-solid border-2  border-black text-[#000000]  rounded-full font-bold">
+                  RUST Developer
+                </div>
+                <div className=" flex justify-center items-center bg-white border-solid border-2  border-black text-[#000000] px-2 rounded-full font-bold">
                   Database Design
                 </div>
-                <div className="bg-white border-solid border-2 border-black text-[#000000] px-1 py-1 rounded-full font-bold">
-                  Database Design Design
-                </div>
-                <div className="bg-white border-solid border-2 border-black text-[#000000] px-5 py-1 rounded-full font-bold">
-                  Database Design Design
-                </div>
-                <div className="bg-white border-solid border-2 border-black text-[#000000] px-5 py-1 rounded-full font-bold">
-                  Database Design
+                <div className=" flex justify-center items-center bg-white border-solid border-2 border-black text-[#000000]  rounded-full font-bold">
+                  Database
                 </div>
               </div>
             </div>
