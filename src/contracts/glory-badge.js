ec93a47dd3499f7f0ec6e9d390a2e71ae144e463
Sordgom <aymaneigmiden@gmail.com>
2022-10-29 15:08:46 +0800

    Added contracts encapsulation

---
+++ src/contracts/glory-badge.js
@@ -0,0 +1,18 @@
+export class GloryBadge {
+    constructor({ contractId, walletToUse }) {
+      this.contractId = contractId;
+      this.wallet = walletToUse;    
+    }
+    
+    async getToken() {
+        return await this.wallet.viewMethod({ contractId: this.contractId, method: 'nft_token' });
+    }
+
+    async getMetadata() {
+        return await this.wallet.viewMethod({ contractId: this.contractId, method: 'nft_metadata' });
+    }
+
+    async mintNft(tokenId, Metadata, receiverId) {
+        return await this.wallet.callMethod({ contractId: this.contractId, method: 'nft_mint', args: { token_id: tokenId, metadata: Metadata, receiver_id: receiverId } });
+    }
+}
